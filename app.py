4e238c546767d8fad15ac5b7e67166f6d7d6ee25
Keh Omoloja <kehomoloja@mwc-110021.dhcp.missouri.edu>
2024-11-01 11:48:11 -0500

    connected and create delete page

--- app.py
+++ app.py
@@ -100,4 +100,24 @@ def edit(id):
     # if Get then display page
 
     return render_template('edit.html', post=post) 
+
+# create a route to delete a post from the edit page
+# post id is the url parameter
+@app.route('/<int:id>/delete/', methods = ('POST',))
+def delete(id):
+    #get the post 
+    post = get_post(id)
+
+    #connect to the database
+    conn = get_db_connection()
+
+    conn.execute('DELETE from posts WHERE id = ?', (id,))
+
+    conn.commit()
+    conn.close()
+
+    flash('"{}" was successfully deleted! '.format(post['title']))
+    #redirect to the home page
+    return redirect(url_for('index'))
+
 app.run(port=5000)
